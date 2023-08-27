b1a6caa3585ea090927dc972c50e53dfb4721c40
gennadiy-gaysha <gennadiy-gaysha@gmail.com>
2023-08-27 21:23:29 +0200

    Add ALLOWED_HOSTS to settings.py

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "django-todo-app-gaysha-repeat-8ee4d1028769.herokuapp.com"]
 
 # Application definition
 
