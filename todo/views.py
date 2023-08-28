fe17c27b74b330b3b83eba5417aa788275e15cf7
gennadiy-gaysha <gennadiy-gaysha@gmail.com>
2023-08-28 17:02:39 +0200

    Create template form for adding a new Item

--- todo/views.py
+++ todo/views.py
@@ -12,3 +12,6 @@ def get_todo_list(request):
     items = Item.objects.all()
     context = {'items': items}
     return render(request, 'todo/todo_list.html', context)
+
+def add_item(request):
+    return render(request, 'todo/add_item.html')
