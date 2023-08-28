62ba3a5da4e6c40bdb11c98e0320f169c58a0466
gennadiy-gaysha <gennadiy-gaysha@gmail.com>
2023-08-28 17:58:29 +0200

    Add "Add Item" function on submitting the form

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
 # from django.shortcuts import render, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Item
 
 # Create your views here.
@@ -14,4 +14,13 @@ def get_todo_list(request):
     return render(request, 'todo/todo_list.html', context)
 
 def add_item(request):
+    # Check if the request method is POST (form submission)
+    if request.method == 'POST':
+        # Get the value of the 'item_name' field from the form
+        name = request.POST.get('item_name')
+        # Check if the 'done' checkbox is selected in the form
+        status = 'done' in request.POST
+        # Create a new Item object with the provided data
+        Item.objects.create(name=name, status=status)
+        return redirect('get_todo_list')
     return render(request, 'todo/add_item.html')
