d210bab1e2c789d8756d0299f58cd14e21437038
gennadiy-gaysha <gennadiy-gaysha@gmail.com>
2023-08-28 14:39:53 +0200

    Create and register Item model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class Item(models.Model):
+    # The null equals false attribute here prevents items from being created without a name
+    # programmatically
+    # and blank equals false will make the field required on forms.
+    # This way we're certain that a todo item can't be created without a name
+    # whether it's done in Python code. Or by a user in a web form or even an administrator in the
+    # admin panel.
+    name = models.CharField(max_length=50, null=False, blank=False)
+    status = models.BooleanField(null=False, blank=False)
+
+    def __str__(self):
+        return f'{self.name} - {self.status}'
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,7 @@
 # from django.shortcuts import render, HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
 
 # def say_hello(request):
